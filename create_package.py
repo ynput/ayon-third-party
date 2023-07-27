7eb2f806e2406c64e761fcfb50f2b6bbb1015fc0
Jakub Trllo <jakub.trllo@gmail.com>
2023-07-27 15:54:37 +0200

    create package fixes

--- create_package.py
+++ create_package.py
@@ -329,7 +329,7 @@ def create_server_package(
                 continue
 
             dst_root = "addon"
-            if root != addon_output_dir_offset:
+            if root != addon_output_dir:
                 dst_root = os.path.join(
                     dst_root, root[addon_output_dir_offset:]
                 )
@@ -407,8 +407,8 @@ if __name__ == "__main__":
         dest="skip_zip",
         action="store_true",
         help=(
-            "Directory path where package will be created"
-            " (Will be purged if already exists!)"
+            "Skip zipping server package and create only"
+            " server folder structure."
         )
     )
     parser.add_argument(
@@ -416,8 +416,7 @@ if __name__ == "__main__":
         dest="keep_sources",
         action="store_true",
         help=(
-            "Directory path where package will be created"
-            " (Will be purged if already exists!)"
+            "Keep folder structure when server package is created."
         )
     )
     parser.add_argument(
