f9c01a8260988b1ab7f29f01c762dd1a294a3439
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-08 14:48:09 +0200

    bump version to '1.1.1-dev.1'

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Package declaring addon version."""
 name = "ayon_third_party"
-version = "1.1.0"
+version = "1.1.1-dev.1"
 title = "3rd Party Distribution"
 client_dir = "ayon_third_party"
