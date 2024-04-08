d36fc3a60bc1e5ef5c9bf0356fc522a9b19541eb
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-08 14:05:48 +0200

    use new structure of addon package (#5)

---
+++ package.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Package declaring addon version."""
+name = "ayon_third_party"
+version = "1.0.1-dev.1"
+title = "3rd Party Distribution"
+client_dir = "ayon_third_party"
